6c5a72acad6c2a0eb59205d8193af4fa95c142b7
jane11117 <s6752c10017@sau.ac.th>
2025-01-19 17:20:12 +0700

    v.2 create BMI project and BMI Project 2

--- src/com/sau/workshop/BMIProject2.java
+++ src/com/sau/workshop/BMIProject2.java
@@ -6,21 +6,22 @@ package com.sau.workshop;
 
 import java.text.DecimalFormat;
 import java.util.Scanner;
+
 /**
  *
  * @author IT306PC
  */
 public class BMIProject2 {
-    
-     public static void main(String[] args){
-         String yourName;
-         double yourWeight, yourHeight, yourBMI;
-         
-         Scanner sc = new Scanner(System.in);
-         DecimalFormat df = new DecimalFormat("#.0000");
-         
-         System.out.println("++++++++++++++++++++++");
-         System.out.println("   BMI Program by DTI-SAU");
+
+    public static void main(String[] args) {
+        String yourName;
+        double yourWeight, yourHeight, yourBMI;
+
+        Scanner sc = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("#.0000");
+
+        System.out.println("++++++++++++++++++++++");
+        System.out.println("   BMI Program by DTI-SAU");
         System.out.println("++++++++++++++++++++++++++++++");
         System.out.print("Input your name: ");
         yourName = sc.nextLine();
@@ -30,9 +31,9 @@ public class BMIProject2 {
         yourHeight = sc.nextDouble();
         System.out.println("++++++++++++++++++++++++++++++");
 
-       yourBMI = yourWeight / Math.pow(yourHeight,2);
+        yourBMI = yourWeight / Math.pow(yourHeight, 2);
         System.out.println("BMI of" + yourName + "is" + df.format("yourBMI"));
         System.out.println("++++++++++++++++++++++++++++++");
-       
-}
+        System.out.println("++++++++++++++++++++++++++++++");
+    }
 }
